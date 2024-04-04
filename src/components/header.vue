18670747f37cb6f740c3898e5825e3bd6f51ec08
galatsan-ip <ivan.galatsan.01@mail.ru>
2024-04-04 11:47:39 +0700

    ssssss tovarchik

--- src/components/header.vue
+++ src/components/header.vue
@@ -3,7 +3,6 @@
 </script>
 
 <template>
-<div>шапка сайта</div>
  <header>
    <nav>
      <ul>
--- src/views/ProductView.vue
+++ src/views/ProductView.vue
@@ -1,11 +1,50 @@
-<script setup>
-
-</script>
-
 <template>
-<div>Товары</div>
+  <h2> Товары</h2>
+  <div class="products">
+    <div class="product" v-for="product in products">
+      <div>{{ product.title }}</div>
+      <div><img :src="product.thumbnail" alt="{{product.title}}"></div>
+      <div class="price">
+        <div>{{product.price}}</div>
+        <div><button class="btn btn-purchase">Купить</button></div>
+      </div>
+    </div>
 </template>
 
-<style scoped>
 
+<style scoped>
+.products{
+  display: flex;
+  flex-wrap: wrap;
+
+}
+.product{
+  width: 300px;
+  margin: 40px 20px;
+}
+.product.img{
+  width: 280px;
+  margin: 10px;
+  background: cadetblue;
+}
 </style>
+
+<script setup>
+export default {
+  data(){
+    return{
+      products: []
+    }
+  },
+  created() {
+    this.getProducts();
+  },
+  methods: {
+    getProducts(){
+      fetch('https://dummyjson.com/products')
+          .then(response => response.json())
+          .then(data => this.products = data.products)
+    }
+  },
+}
+</script>
